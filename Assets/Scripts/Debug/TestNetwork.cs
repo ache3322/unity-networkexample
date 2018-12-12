c8283d0299a33bbac3902dc2be6b2557df3ec961
Austin <acheese1234@gmail.com>
2018-12-12 01:25:39 -0500

    test game logic

---
+++ Assets/Scripts/Debug/TestNetwork.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestNetwork : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //DevLog.Log("TestNetwork", "is in update");
+    }
+}
--- Assets/Scripts/GameSettings/NetworkManagerCustom.cs
+++ Assets/Scripts/GameSettings/NetworkManagerCustom.cs
@@ -5,5 +5,35 @@ using UnityEngine.Networking;
 
 public class NetworkManagerCustom : NetworkManager
 {
+    static public NetworkManagerCustom s_Singleton;
 
+
+    //Client numPlayers from NetworkManager is always 0, so we count (throught connect/destroy in LobbyPlayer) the number
+    //of players, so that even client know how many player there is.
+    [HideInInspector]
+    public int _playerNumber = 0;
+
+
+
+    void Start()
+    {
+        s_Singleton = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        DevLog.Log("NetworkManagerCustom", "Someone connected to the server: " + conn.connectionId);
+    }
+
+    //allow to handle the (+) button to add/remove player
+    public void OnPlayersNumberModified(int count)
+    {
+        _playerNumber += count;
+
+        int localPlayerCount = 0;
+        foreach (UnityEngine.Networking.PlayerController p in ClientScene.localPlayers)
+            localPlayerCount += (p == null || p.playerControllerId == -1) ? 0 : 1;
+    }
 }
